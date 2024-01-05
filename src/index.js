955863b7f62da7dc91cc5bcdea2ea87c55584a9b
Romell Bermundo <romell.bermundo@outlook.com>
2024-01-04 20:49:59 -0700

    card component version 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import Card from "./Card";
 
 ReactDOM.render(<Card />, document.getElementById("root"));
-registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
