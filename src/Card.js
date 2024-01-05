848c1e2bcce93ef66df430557f50e6c3cc8e2f20
Romell Bermundo <romell.bermundo@outlook.com>
2024-01-04 20:42:17 -0700

    initial card component

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,13 @@ import React from "react";
 const Card = () => {
   return (
     <div>
-      <img src="" />
+      <img alt="robots" src="" />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
     </div>
   );
 };
+
+export default Card;
